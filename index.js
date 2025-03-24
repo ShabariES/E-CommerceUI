53b8a5be5cb1fd13d110e15d9d2810486940562c
ShabariES <shanmugame1212@gmail.com>
2025-03-24 10:11:28 +0530

    Add files via upload
    I made an Ui design for an fashion style by using html,css,js and made an login with OTP verification by E-mail by using emailjs library

---
+++ index.js
@@ -0,0 +1,9 @@
+var sides =document.querySelector(".side")
+//sides.style.display="none"
+function shownav(){
+sides.style.display="block"
+}
+function closenav(){
+    sides.style.display="none"
+    }
+    
